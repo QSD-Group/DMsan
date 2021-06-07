2f4a3e9ef7b92e0bd4ac533834b235462409e65e
Yalin Li <zoe.yalin.li@gmail.com>
2021-06-07 18:32:45 -0500

    add code for LCA uncertainty analysis

--- env.py
+++ env.py
@@ -9,6 +9,12 @@ TODO: figure out why ReCiPe gives so much credit to fertilizers
 
 """
 
+# %%
+
+# =============================================================================
+# Baseline for Individualist, Hierarchist, and Egalitarian (I/H/E)
+# =============================================================================
+
 from exposan import bwaise as bw
 bw.update_lca_data('new')
 
@@ -24,9 +30,34 @@ for cat in ('I', 'H', 'E'):
 for lca in lcas:
     lca.indicators = indicators
 
-def get_annualized_pts(lca):
+
+def get_cap_yr_pts(lca):
     impact_dct = lca.get_total_impacts()
     ratio = lca.lifetime * get_ppl(lca.system.ID[-1])
     for k, v in impact_dct.items():
         impact_dct[k] = v / ratio
     return impact_dct
+
+
+# To get some results
+baseline_dct = {}
+for lca in lcas:
+    baseline_dct[lca.system.ID] = get_cap_yr_pts(lca)
+
+
+# %%
+
+# =============================================================================
+# Add uncertainties
+# =============================================================================
+
+from exposan.bwaise.models import update_metrics, update_LCA_CF_parameters, run_uncertainty
+models = modelA, modelB, modelC = bw.modelA, bw.modelB, bw.modelC
+
+for model in models:
+    model = update_LCA_CF_parameters(model, 'new')
+    model = update_metrics(model, 'new')
+
+uncertainty_dct = {}
+for model in models:
+    uncertainty_dct[model.system.ID] = run_uncertainty(model, N=10)
