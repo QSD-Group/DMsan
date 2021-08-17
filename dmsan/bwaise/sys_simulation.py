302f1c653904ff0804314da0ee1e1422b54ddb67
Yalin <zoe.yalin.li@gmail.com>
2021-08-16 22:48:10 -0500

    dmsan/bwaise/scores/parameters.xlsx

--- dmsan/bwaise/sys_simulation.py
+++ dmsan/bwaise/sys_simulation.py
@@ -5,19 +5,81 @@ Created on Sun Jun  6 13:31:44 2021
 
 @author: Yalin Li <zoe.yalin.li@gmail.com>
 
+Run this module to save the results to the /data folder to avoid repeating
+simulating the system.
 """
 
-# Run this module to save the results to the /data folder to avoid repeating
-# simulating the system
 
-import os
+import os, pickle
 import numpy as np
 import pandas as pd
 from qsdsan.utils.decorators import time_printer
 from exposan import bwaise as bw
 from dmsan.bwaise import scores_path
 
-__all__ = ('get_baseline', 'save_baseline', 'get_uncertainties')
+__all__ = ('rebuild_models', 'get_baseline', 'get_uncertainties')
+
+
+# %%
+
+# =============================================================================
+# Util functions for reloading saved models
+# =============================================================================
+
+def get_model(N, seed=None, rule='L', lca_perspective='H'):
+    from exposan.bwaise.models import update_metrics, update_LCA_CF_parameters
+    models = modelA, modelB, modelC = bw.modelA, bw.modelB, bw.modelC
+
+    bw.update_lca_data('new')
+    if seed:
+        np.random.seed(seed)
+
+    pers = ['I', 'H', 'E']
+    pers.remove(lca_perspective)
+    for model in models:
+        model = update_LCA_CF_parameters(model, 'new')
+
+        # Only do ReCiPe, hierarchist (H) perspective
+        model.set_parameters([i for i in model.parameters
+                              if not (f' {pers[0]} ' in i.name or
+                                      f' {pers[1]} ' in i.name or
+                                      'global warming' in i.name)])
+
+        model = update_metrics(model, 'new')
+
+        get_metric = lambda name: [i for i in model.metrics if i.name==name]
+        mRR = sum([get_metric(f'Total {i}') for i in ('N', 'P', 'K')], [])
+        # mRR = sum([get_metric(f'Total {i}') for i in ('N', 'P', 'K', 'COD')], [])
+        mRR += get_metric('Gas COD') # energy, only gas
+        mEnv = [i for i in model.metrics
+                if ('Net emission' in i.name and f'{lca_perspective}_' in i.name)]
+        mEnv.sort(key=lambda i: i.name_with_units)
+        mEcon = get_metric('Annual net cost')
+        model.metrics = mRR + mEnv + mEcon
+
+        samples = model.sample(N, rule)
+        model.load_samples(samples)
+
+    copy_samples(modelA, modelB)
+    copy_samples(modelA, modelC)
+    copy_samples(modelB, modelC, exclude=modelA.parameters)
+
+    return modelA, modelB, modelC
+
+
+def rebuild_models(path=''):
+    path = path if path else os.path.join(scores_path, 'model_data.pckl')
+    f = open(path, 'rb')
+    data = pickle.load(f)
+    f.close()
+
+    modelA, modelB, modelC = get_model(*data['inputs'])
+    modelA._samples, modelB._samples, modelC._samples = data['samples']
+    modelA.table, modelB.table, modelC.table = data['tables']
+
+    return modelA, modelB, modelC
+
+# modelA, modelB, modelC = rebuild_models()
 
 
 # %%
@@ -50,7 +112,7 @@ def get_cap_yr_pts(lca):
     return impact_dct
 
 
-def get_baseline():
+def get_baseline(file_path=''):
     baseline_dct = {'sysA': [], 'sysB': [], 'sysC': []}
     lcas = update_lca()
     inds = lcas[0].indicators
@@ -76,18 +138,15 @@ def get_baseline():
         ppl = sys_dct['ppl'][sys.ID]
         data.append(func_dct['get_annual_net_cost'](tea, ppl))
         df[sys.ID] = data
+
+    if file_path:
+        sep = '\t' if file_path.endswith('.tsv') else ''
+
+        df.to_csv(file_path, sep=sep)
     return df
 
-
-def save_baseline(file_path=''):
-    if not file_path:
-        file_path = os.path.join(scores_path, 'sys_baseline.tsv')
-    sep = '\t' if file_path.endswith('.tsv') else ''
-    baseline_df = get_baseline()
-    baseline_df.to_csv(file_path, sep=sep)
-    return baseline_df
-
-baseline_df = save_baseline()
+baseline_path = os.path.join(scores_path, 'sys_baseline.tsv')
+baseline_df = get_baseline(file_path=baseline_path)
 
 
 # %%
@@ -112,41 +171,47 @@ def copy_samples(original, new, exclude=()):
 
 
 @time_printer
-def get_uncertainties(N, seed=None, rule='L', file_path=''):
-    from exposan.bwaise.models import update_metrics, update_LCA_CF_parameters
-    models = modelA, modelB, modelC = bw.modelA, bw.modelB, bw.modelC
+def get_uncertainties(N, seed=None, rule='L', lca_perspective='H',
+                      pickle_path='', param_path='', result_path=''):
+    # from exposan.bwaise.models import update_metrics, update_LCA_CF_parameters
+    # models = modelA, modelB, modelC = bw.modelA, bw.modelB, bw.modelC
 
-    bw.update_lca_data('new')
-    if seed:
-        np.random.seed(seed)
+    # bw.update_lca_data('new')
+    # if seed:
+    #     np.random.seed(seed)
 
+    # pers = ['I', 'H', 'E']
+    # pers.remove(lca_perspective)
+    # uncertainty_dct = {}
+    # for model in models:
+    #     model = update_LCA_CF_parameters(model, 'new')
+
+    #     # Only do ReCiPe, hierarchist (H) perspective
+    #     model.set_parameters([i for i in model.parameters
+    #                           if not (f' {pers[0]} ' in i.name or
+    #                                   f' {pers[1]} ' in i.name or
+    #                                   'global warming' in i.name)])
+
+    #     model = update_metrics(model, 'new')
+
+    #     get_metric = lambda name: [i for i in model.metrics if i.name==name]
+    #     mRR = sum([get_metric(f'Total {i}') for i in ('N', 'P', 'K')], [])
+    #     # mRR = sum([get_metric(f'Total {i}') for i in ('N', 'P', 'K', 'COD')], [])
+    #     mRR += get_metric('Gas COD') # energy, only gas
+    #     mEnv = [i for i in model.metrics
+    #             if ('Net emission' in i.name and f'{lca_perspective}_' in i.name)]
+    #     mEnv.sort(key=lambda i: i.name_with_units)
+    #     mEcon = get_metric('Annual net cost')
+    #     model.metrics = mRR + mEnv + mEcon
+
+    #     samples = model.sample(N, rule)
+    #     model.load_samples(samples)
+
+    # copy_samples(modelA, modelB)
+    # copy_samples(modelA, modelC)
+    # copy_samples(modelB, modelC, exclude=modelA.parameters)
+    models = get_model(N, seed, rule, lca_perspective)
     uncertainty_dct = {}
-    for model in models:
-        model = update_LCA_CF_parameters(model, 'new')
-
-        # Only do ReCiPe, hierarchist (H) perspective
-        model.set_parameters([i for i in model.parameters
-                              if not (' I ' in i.name or
-                                      ' E ' in i.name or
-                                      'global warming' in i.name)])
-
-        model = update_metrics(model, 'new')
-
-        get_metric = lambda name: [i for i in model.metrics if i.name==name]
-        mRR = sum([get_metric(f'Total {i}') for i in ('N', 'P', 'K')], [])
-        # mRR = sum([get_metric(f'Total {i}') for i in ('N', 'P', 'K', 'COD')], [])
-        mRR += get_metric('Gas COD') # energy, only gas
-        mEnv = [i for i in model.metrics if ('Net emission' in i.name and 'H_' in i.name)]
-        mEnv.sort(key=lambda i: i.name_with_units)
-        mEcon = get_metric('Annual net cost')
-        model.metrics = mRR + mEnv + mEcon
-
-        samples = model.sample(N, rule)
-        model.load_samples(samples)
-
-    copy_samples(modelA, modelB)
-    copy_samples(modelA, modelC)
-    copy_samples(modelB, modelC, exclude=modelA.parameters)
 
     for model in models:
         df = model.table
@@ -163,14 +228,45 @@ def get_uncertainties(N, seed=None, rule='L', file_path=''):
         uncertainty_dct[f'{model.system.ID}-results'] = \
             model.table.iloc[:, len(model.parameters):]
 
-    if file_path:
-        writer = pd.ExcelWriter(file_path)
+    if param_path:
+        dfs = dict.fromkeys(('Alternative A', 'Alternative B', 'Alternative C'))
+        for model in models:
+            df = dfs[f'Alternative {model.system.ID[-1]}'] = pd.DataFrame()
+            df['Parameters'] = [i for i in model.table.columns[:len(model.parameters)].sort_values()]
+            df['DV'] = df['T'] = df['RR'] = df['Env'] = df['Econ'] = df['S'] = ''
+
+        writer = pd.ExcelWriter(param_path)
+        for sys_ID, df in dfs.items():
+            df.to_excel(writer, sheet_name=sys_ID)
+        writer.save()
+
+    if pickle_path:
+        # Cannot just save the `Model` object as a pickle file
+        # because it contains local functions
+        data = {
+            'inputs': [N, seed, rule, lca_perspective],
+            'samples': [i._samples for i in models],
+            'tables': [i.table for i in models]
+            }
+        f = open(pickle_path, 'wb')
+        pickle.dump(data, f)
+        f.close()
+
+    if result_path:
+        writer = pd.ExcelWriter(result_path)
         for sys_ID, df in uncertainty_dct.items():
             df.to_excel(writer, sheet_name=sys_ID)
         writer.save()
 
     return uncertainty_dct
 
-file_path = os.path.join(scores_path, 'sys_uncertainties.xlsx')
-# file_path = '' # if don't want to save the file but want to see the results
-uncertainty_dct = get_uncertainties(N=1000, seed=3221, file_path=file_path)
+
+param_path = os.path.join(scores_path, 'parameters.xlsx')
+pickle_path = os.path.join(scores_path, 'model_data.pckl')
+uncertainty_path = os.path.join(scores_path, 'sys_uncertainties.xlsx')
+
+# pickle_path = excel_path = '' # if don't want to save the results
+uncertainty_dct = get_uncertainties(N=1000, seed=3221,
+                                    param_path=param_path,
+                                    pickle_path=pickle_path,
+                                    result_path=uncertainty_path)
