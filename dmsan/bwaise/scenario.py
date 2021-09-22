c7554b7d87c9add3c10149b6e935f9fab73beacc
Yalin <zoe.yalin.li@gmail.com>
2021-09-21 23:09:58 -0500

    add functions for accumulated scenarios

--- dmsan/bwaise/scenario.py
+++ dmsan/bwaise/scenario.py
@@ -10,9 +10,8 @@ the performance score of each system and thus the final winner.
 """
 
 import os
-import numpy as np, pandas as pd, seaborn as sns
+import numpy as np, pandas as pd
 from matplotlib import pyplot as plt
-from qsdsan.utils import time_printer
 from dmsan import AHP
 from dmsan.bwaise import results_path, figures_path, import_from_pickle
 
@@ -46,6 +45,7 @@ def update_local_weights(tech_scores):
     ahp.get_AHP_weights(True)
 
     return ahp.norm_weights_df
+
 
 # %%
 
@@ -103,8 +103,8 @@ def test_oat(mcda, alt, indicators=None, best_score={}):
         except TypeError:
             indicators = (indicators,)
 
-    test_dct = {ind: {} for ind in indicators}
-    for ind, data in test_dct.items():
+    oat_dct = {ind: {} for ind in indicators}
+    for ind, data in oat_dct.items():
         # Reset technology scores and refresh results
         mcda.tech_scores = baseline_tech_scores.copy()
         series = mcda.tech_scores.loc[:, ind]
@@ -143,126 +143,191 @@ def test_oat(mcda, alt, indicators=None, best_score={}):
         data['rank after updating'] = updated_rank
         data['winning chance'] = win_chance
 
-    # Get the winning chance for all best scores at the same time
-    for ind, data in test_dct.items():
-        updated = data['updated']
-        mcda.tech_scores.loc[idx, ind] = updated
+    # # Get the winning chance for all best scores at the same time
+    # for ind, data in test_dct.items():
+    #     updated = data['updated']
+    #     mcda.tech_scores.loc[idx, ind] = updated
 
-    mcda.indicator_weights = update_local_weights(mcda.tech_scores)
-    mcda.run_MCDA()
-    test_dct['all at once'] = {'winning chance':
-                               mcda.winners[mcda.winners.Winner==alt].shape[0]/weight_num}
+    # mcda.indicator_weights = update_local_weights(mcda.tech_scores)
+    # mcda.run_MCDA()
+    # test_dct['all at once'] = {'winning chance':
+    #                            mcda.winners[mcda.winners.Winner==alt].shape[0]/weight_num}
 
     # Get the winning chance at baseline values
     mcda.tech_scores = baseline_tech_scores.copy()
     mcda.indicator_weights = baseline_local_weights.copy()
     mcda.run_MCDA()
-    test_dct['baseline'] = {'winning chance':
+    oat_dct['baseline'] = {'winning chance':
                             mcda.winners[mcda.winners.Winner==alt].shape[0]/weight_num}
 
-    return test_dct
+    return oat_dct
 
 
-def plot_oat(test_dct):
-    labels, winning = ['baseline'], [test_dct['baseline']['winning chance']]
+def plot_oat(oat_dct, save=True):
+    labels, winning = ['baseline'], [oat_dct['baseline']['winning chance']]
     get_rounded = lambda val: round(val, 2) if len(str(val).split('.')[-1]) >= 2 else val
 
-    for ind, data in test_dct.items():
-        if not ind in ('baseline', 'all at once'):
+    for ind, data in oat_dct.items():
+        if not ind == 'baseline':
+        # if not ind in ('baseline', 'all at once'):
             labels.append(f'{ind}: {get_rounded(data["baseline"])}->{get_rounded(data["updated"])}')
             winning.append(data['winning chance'])
 
-    labels.append('all at once')
-    winning.append(test_dct['all at once']['winning chance'])
+    # labels.append('all at once')
+    # winning.append(test_dct['all at once']['winning chance'])
 
     fig, ax = plt.subplots(figsize=(6, 8))
     y = np.arange(len(labels))
     ax.barh(y, winning, align='center')
-    ax.set_yticks(y)
-    ax.set_yticklabels(labels)
+    ax.set(yticks=y, yticklabels=labels, ylabel='Changed indicator (one-at-a-time)',
+           xlabel='Winning chance')
     ax.invert_yaxis()  # labels read top-to-bottom
-    ax.set_xlabel('Winning chance')
 
     fig.subplots_adjust(left=0.5)
+
+    if save:
+        fig.savefig(os.path.join(figures_path, 'test_best.png'), dpi=100)
 
     return ax
 
 
-# %%
+def test_acc(mcda, alt, oat_dct):
+    acc_dct = {}
+    baseline_winning = oat_dct['baseline']['winning chance']
+    for ind, data_dct in oat_dct.items():
+        ind_winning = data_dct['winning chance']
+        if (ind_winning == baseline_winning or ind_winning == 1):
+            continue
 
-@time_printer
-def test_across_axis(mcda, alt, indicator, include_simulation_uncertainties=False,
-                     min_val=None, max_val=None, step_num=10):
-    '''Run all global weight scenarios at certain steps with the desired range.'''
-    # Reset technology scores
-    mcda.tech_scores = baseline_tech_scores.copy()
+        acc_dct[ind] = ind_winning
+
+    # Sort the dict in order
+    ind_list = [i for i in acc_dct.keys() if not i in ('baseline', 'all at once')]
+    ind_list.sort(key=lambda ind: acc_dct[ind], reverse=True)
+    ind_list.insert(0, 'baseline')
+    acc_dct['baseline'] = baseline_winning
+    acc_dct = {ind:acc_dct[ind] for ind in ind_list}
+
+    weight_num = mcda.criteria_weights.shape[0]
     idx = mcda.alt_names[mcda.alt_names==alt].index[0]
 
-    min_val = min_val if min_val else 0
-    max_val = max_val if max_val else baseline_tech_scores.loc[idx, indicator]
-
-    # Total number of global weights
-    weight_num = mcda.criteria_weights.shape[0]
-
-    vals = np.linspace(min_val, max_val, num=step_num)
-    win_dct = {}
-    if not include_simulation_uncertainties: # fix other tech scores at the baseline
-
-        for val in vals:
-            mcda.tech_scores.loc[idx, indicator] = val
+    for ind in acc_dct.keys():
+        if ind == 'baseline':
+            pass
+        else:
+            mcda.tech_scores.loc[idx, ind] = oat_dct[ind]['updated']
+            mcda.indicator_weights = update_local_weights(mcda.tech_scores)
             mcda.run_MCDA()
-            win_dct[val] = mcda.winners[mcda.winners.Winner==alt].shape[0]/weight_num
+            acc_dct[ind] = mcda.winners[mcda.winners.Winner==alt].shape[0]/weight_num
 
-    else:
-        # Make a copy for manipulating
-        tech_score_dct_updated = tech_score_dct.copy()
-        for val in vals:
-            # Update all the costs to be the set value
-            for v in tech_score_dct_updated.values():
-                tech_score_dct_updated.loc[idx, indicator] = val
-            score_df_dct, rank_df_dct, winner_df = \
-                mcda.run_MCDA_multi_scores(tech_score_dct=tech_score_dct_updated)
-            win_dct[val] = winner_df[winner_df==alt].count()/weight_num
-
-    return win_dct
+    return acc_dct
 
 
-def plot_across_axis(win_dct, include_simulation_uncertainties=False):
-    if not include_simulation_uncertainties:
-        fig, ax = plt.subplots(figsize=(8,4.5))
-        ax.plot(win_dct.keys(), win_dct.values())
-    if include_simulation_uncertainties:
-        dfs = []
-        for val, win in win_dct.items():
-            df_val = pd.DataFrame()
-            df_val['win'] = win
-            df_val['val'] = val
-            dfs.append(df_val)
-        df = pd.concat(dfs)
-        df.reset_index(drop=True, inplace=True)
-        ax = sns.lineplot(x=df['val'], y=df['win'])
+def plot_acc(acc_dct, save=True):
+    fig, ax = plt.subplots(figsize=(6, 8))
+    labels = [f'+{i}' for i in acc_dct.keys()]
+    labels[0] = labels[0].lstrip('+')
+
+    x = np.arange(len(labels))
+    ax.plot(x, acc_dct.values(), '-o')
+
+    ax.set(xticks=x, xticklabels=labels, xlabel='Changed indicator (accumulated)',
+           ylabel='Winning chance')
+
+    for label in ax.get_xticklabels():
+        label.set_rotation(30)
+    if save:
+        fig.savefig(os.path.join(figures_path, 'test_acc.png'), dpi=100)
+
     return ax
 
 
 # %%
 
 if __name__ == '__main__':
-    # # 100 steps take less than 1 min
-    # Cwin_across_cost = test_across_axis(bwaise_mcda, alt='Alternative C',
-    #                                     indicator='Econ1',
-    #                                     include_simulation_uncertainties=False,
-    #                                     min_val=0, step_num=100)
-    # ax0 = plot_across_axis(Cwin_across_cost, include_simulation_uncertainties=False)
-    # ax0.figure.savefig(os.path.join(figures_path, 'Cwin_across_cost.png'), dpi=100)
+    oat_dct = test_oat(bwaise_mcda, alt='Alternative C', best_score=best_score_dct)
+    ax = plot_oat(oat_dct, save=True)
 
-    # # **Each** step takes ~1 hour (at each step, we are runnin 1000 simulation*1000 global weights)
-    # Cwin_across_cost_uncertainty = test_across_axis(bwaise_mcda, alt='Alternative C',
-    #                                                 indicator='Econ1',
-    #                                                 include_simulation_uncertainties=True,
-    #                                                 min_val=0, step_num=2)
-    # ax1 = plot_across_axis(Cwin_across_cost_uncertainty, include_simulation_uncertainties=True)
-    # ax1.figure.savefig(os.path.join(figures_path, 'Cwin_across_cost_with_band.png'), dpi=100)
+    acc_dct = test_acc(bwaise_mcda, alt='Alternative C', oat_dct=oat_dct)
+    ax = plot_acc(acc_dct, save=True)
 
-    test_dct_best = test_oat(bwaise_mcda, alt='Alternative C', best_score=best_score_dct)
-    ax = plot_oat(test_dct_best)
-    ax.figure.savefig(os.path.join(figures_path, 'test_best.png'), dpi=100)
+
+
+# %%
+
+# =============================================================================
+# Legacy codes not in use
+# =============================================================================
+
+# import seaborn as sns
+# from qsdsan.utils import time_printer
+
+# @time_printer
+# def test_across_axis(mcda, alt, indicator, include_simulation_uncertainties=False,
+#                      min_val=None, max_val=None, step_num=10):
+#     '''Run all global weight scenarios at certain steps with the desired range.'''
+#     # Reset technology scores
+#     mcda.tech_scores = baseline_tech_scores.copy()
+#     idx = mcda.alt_names[mcda.alt_names==alt].index[0]
+
+#     min_val = min_val if min_val else 0
+#     max_val = max_val if max_val else baseline_tech_scores.loc[idx, indicator]
+
+#     # Total number of global weights
+#     weight_num = mcda.criteria_weights.shape[0]
+
+#     vals = np.linspace(min_val, max_val, num=step_num)
+#     win_dct = {}
+#     if not include_simulation_uncertainties: # fix other tech scores at the baseline
+
+#         for val in vals:
+#             mcda.tech_scores.loc[idx, indicator] = val
+#             mcda.run_MCDA()
+#             win_dct[val] = mcda.winners[mcda.winners.Winner==alt].shape[0]/weight_num
+
+#     else:
+#         # Make a copy for manipulating
+#         tech_score_dct_updated = tech_score_dct.copy()
+#         for val in vals:
+#             # Update all the costs to be the set value
+#             for v in tech_score_dct_updated.values():
+#                 tech_score_dct_updated.loc[idx, indicator] = val
+#             score_df_dct, rank_df_dct, winner_df = \
+#                 mcda.run_MCDA_multi_scores(tech_score_dct=tech_score_dct_updated)
+#             win_dct[val] = winner_df[winner_df==alt].count()/weight_num
+
+#     return win_dct
+
+
+# def plot_across_axis(win_dct, include_simulation_uncertainties=False):
+#     if not include_simulation_uncertainties:
+#         fig, ax = plt.subplots(figsize=(8,4.5))
+#         ax.plot(win_dct.keys(), win_dct.values())
+#     if include_simulation_uncertainties:
+#         dfs = []
+#         for val, win in win_dct.items():
+#             df_val = pd.DataFrame()
+#             df_val['win'] = win
+#             df_val['val'] = val
+#             dfs.append(df_val)
+#         df = pd.concat(dfs)
+#         df.reset_index(drop=True, inplace=True)
+#         ax = sns.lineplot(x=df['val'], y=df['win'])
+#     return ax
+
+# if __name__ == '__main__':
+#     # 100 steps take less than 1 min
+#     Cwin_across_cost = test_across_axis(bwaise_mcda, alt='Alternative C',
+#                                         indicator='Econ1',
+#                                         include_simulation_uncertainties=False,
+#                                         min_val=0, step_num=100)
+#     ax0 = plot_across_axis(Cwin_across_cost, include_simulation_uncertainties=False)
+#     ax0.figure.savefig(os.path.join(figures_path, 'Cwin_across_cost.png'), dpi=100)
+
+#     # **Each** step takes ~1 hour (at each step, we are runnin 1000 simulation*1000 global weights)
+#     Cwin_across_cost_uncertainty = test_across_axis(bwaise_mcda, alt='Alternative C',
+#                                                     indicator='Econ1',
+#                                                     include_simulation_uncertainties=True,
+#                                                     min_val=0, step_num=2)
+#     ax1 = plot_across_axis(Cwin_across_cost_uncertainty, include_simulation_uncertainties=True)
+#     ax1.figure.savefig(os.path.join(figures_path, 'Cwin_across_cost_with_band.png'), dpi=100)
