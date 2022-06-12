eedd11da4f653f783f8753972d478f7980c971d3
Yalin Li <mailto.yalin.li@gmail.com>
2022-06-12 08:53:48 -0500

    rework sys_simulation of bwaise

--- dmsan/bwaise/sys_simulation.py
+++ dmsan/bwaise/sys_simulation.py
@@ -11,7 +11,7 @@ simulating the system.
 import os
 import numpy as np
 import pandas as pd
-from qsdsan.utils import time_printer, load_pickle, save_pickle
+from qsdsan.utils import time_printer, load_pickle, save_pickle, copy_samples
 from exposan import bwaise as bw
 from dmsan.bwaise import scores_path
 
@@ -28,42 +28,19 @@ __all__ = ('rebuild_models', 'get_baseline', 'get_uncertainties')
 # Util functions for reloading saved models
 # =============================================================================
 
-def copy_samples(original, new, exclude=()):
-    '''
-    Copy samples of the shared parameters in the original model to the new model.
-    Parameters in `exclude` will be excluded (i.e., not copied).
-    '''
-    col0 = original.table.columns.get_level_values(1)[:len(original.parameters)]
-    col1 = new.table.columns.get_level_values(1)[:len(new.parameters)]
-    shared = col0.intersection(col1)
-    shared = shared.difference([i.name_with_units for i in exclude])
-    idx0 = original.table.columns.get_locs([slice(None), shared])
-    idx1 = new.table.columns.get_locs([slice(None), shared])
-    new.table[new.table.columns[idx1]] = new._samples[:, idx1] \
-        = original._samples[:, idx0]
-
-
 def get_model(N, seed=None, rule='L', lca_perspective='H'):
-    from exposan.bwaise.models import update_metrics, update_LCA_CF_parameters
-    models = modelA, modelB, modelC = bw.modelA, bw.modelB, bw.modelC
+    models = modelA, modelB, modelC = [bw.create_model(ID, lca_kind='new') for ID in ('A', 'B', 'C')]
 
-    bw.update_lca_data('new')
-    if seed:
-        np.random.seed(seed)
-
+    if seed: np.random.seed(seed)
     pers = ['I', 'H', 'E']
     pers.remove(lca_perspective)
-    for model in models:
-        model = update_LCA_CF_parameters(model, 'new')
 
+    for model in models:
         # Only do ReCiPe, hierarchist (H) perspective
         model.set_parameters([i for i in model.parameters
                               if not (f' {pers[0]} ' in i.name or
                                       f' {pers[1]} ' in i.name or
                                       'global warming' in i.name)])
-
-        model = update_metrics(model, 'new')
-
         get_metric = lambda name: [i for i in model.metrics if i.name==name]
         mRR = sum([get_metric(f'Total {i}') for i in ('N', 'P', 'K')], [])
         # mRR = sum([get_metric(f'Total {i}') for i in ('N', 'P', 'K', 'COD')], [])
@@ -98,77 +75,21 @@ def rebuild_models(path=''):
 # %%
 
 # =============================================================================
-# Baseline for Individualist, Hierarchist, and Egalitarian (I/H/E)
+# Simulation functions
 # =============================================================================
 
-def update_lca():
-    bw.update_lca_data('new')
-    lcas = lcaA, lcaB, lcaC = bw.lcaA, bw.lcaB, bw.lcaC
-
-    indicators = []
-    for cat in ('I', 'H', 'E'):
-        temp = [i for i in lcaA.indicators if cat+'_' in i.ID and '_Total' in i.ID]
-        temp.sort(key=lambda ind: ind.ID)
-        indicators.extend(temp)
-
-    for lca in lcas:
-        lca.indicators = indicators
-
-    return lcas
-
-
-def get_cap_yr_pts(lca):
-    impact_dct = lca.get_total_impacts()
-    ratio = lca.lifetime * bw.systems.get_ppl(lca.system.ID[-1])
-    for k, v in impact_dct.items():
-        impact_dct[k] = v / ratio
-    return impact_dct
-
-
-def get_baseline(file_path=''):
-    baseline_dct = {'sysA': [], 'sysB': [], 'sysC': []}
-    lcas = update_lca()
-    inds = lcas[0].indicators
-    idxs = pd.MultiIndex.from_tuples([
-        *zip(('Net recovery',)*4, ('N', 'P', 'K', 'energy')),
-        *zip(('LCA results',)*len(inds), inds),
-        ('TEA results', 'Net cost'),
-        ])
-    df = pd.DataFrame(index=idxs)
-
-    sys_dct = bw.systems.sys_dct
-    for sys in (bw.sysA, bw.sysB, bw.sysC):
-        data = baseline_dct[sys.ID]
-        func_dct = bw.systems.get_summarizing_functions(sys)
-        for i in ('N', 'P', 'K'):
-            data.append(func_dct[f'get_tot_{i}_recovery'](sys, i))
-        data.append(func_dct['get_gas_COD_recovery'](sys, 'COD')) # energy, only gas
-
-        lca = sys_dct['LCA'][sys.ID]
-        data.extend((i for i in get_cap_yr_pts(lca).values()))
-
-        tea = sys_dct['TEA'][sys.ID]
-        ppl = sys_dct['ppl'][sys.ID]
-        data.append(func_dct['get_annual_net_cost'](tea, ppl))
-        df[sys.ID] = data
-
-    if file_path:
-        sep = '\t' if file_path.endswith('.tsv') else ''
-
-        df.to_csv(file_path, sep=sep)
+def get_baseline(models, path=''):
+    baseline_dct = {}
+    for model in models:
+        baseline_dct[model.system.ID] = model.metrics_at_baseline()
+    df = pd.DataFrame.from_dict(baseline_dct)
+    if path: df.to_csv(path)
     return df
 
 
-# %%
-
-# =============================================================================
-# Add uncertainties
-# =============================================================================
-
 @time_printer
-def get_uncertainties(N, seed=None, rule='L', lca_perspective='H',
+def get_uncertainties(models, N=1000, seed=None, rule='L', lca_perspective='H',
                       pickle_path='', param_path='', result_path=''):
-    models = get_model(N, seed, rule, lca_perspective)
     uncertainty_dct = {}
 
     for model in models:
@@ -212,32 +133,34 @@ def get_uncertainties(N, seed=None, rule='L', lca_perspective='H',
 
     return uncertainty_dct
 
-
-
-
-
-# %%
-
-# =============================================================================
-# Run all simulations
-# =============================================================================
-
-def run_simulations(baseline=False, uncertainty=False, save=False):
+# Wrapper function
+def run_simulations(models=None, baseline=False, uncertainty=False, save=False):
     if save:
-        baseline_path = os.path.join(scores_path, 'sys_baseline.tsv')
+        baseline_path = os.path.join(scores_path, 'sys_baseline.csv')
         param_path = os.path.join(scores_path, 'parameters.xlsx')
         pickle_path = os.path.join(scores_path, 'model_data.pckl')
         uncertainty_path = os.path.join(scores_path, 'sys_uncertainties.xlsx')
     else:
         baseline_path = param_path = pickle_path = uncertainty_path = ''
+
     global baseline_df, uncertainty_dct
-    if baseline: baseline_df = get_baseline(file_path=baseline_path)
+    if baseline: baseline_df = get_baseline(models, path=baseline_path)
     if uncertainty:
-        uncertainty_dct = get_uncertainties(N=1000, seed=3221,
+        N = model_kwargs.get('N', 1000)
+        seed = model_kwargs.get('seed', 3221)
+        uncertainty_dct = get_uncertainties(models, N=N, seed=seed,
                                             param_path=param_path,
                                             pickle_path=pickle_path,
                                             result_path=uncertainty_path)
 
 if __name__ == '__main__':
-    run_simulations(baseline=True, uncertainty=False, save=False)
+    # models = rebuild_models()
+    model_kwargs = dict(N=100, seed=3221, rule='L', lca_perspective='H')
+    models = get_model(**model_kwargs)
+    run_simulations(
+        models=models,
+        baseline=True,
+        uncertainty=True,
+        save=True,
+        )
     # run_simulations(baseline=True, uncertainty=True)
