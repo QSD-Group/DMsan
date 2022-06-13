4bd2afa169794001c4366995457b7981e0b1c4d6
Joy Cheung <joycheung1994@gmail.com>
2022-06-13 15:26:42 -0500

    remove Spearman analysis with rank data

--- dmsan/bwaise/uncertainty_sensitivity.py
+++ dmsan/bwaise/uncertainty_sensitivity.py
@@ -390,17 +390,17 @@ def run_analyses(save_sensitivity_excel=False):
     score_df_dct, rank_df_dct, winner_df = \
         bwaise_mcda.run_MCDA_multi_scores(criterion_weights=weight_df,
                                           ind_score_dct=ind_score_dct)
-
-    #!!! Sensitivity looks weird, might need to rewrite
+    
+    #!!! No longer run Spearman with ranks data
     kind = 'Spearman'
-    global score_corr_dct, rank_corr_dct
+    global score_corr_dct
     score_corr_dct = run_uncertainty_corr(score_df_dct, kind)
-    rank_corr_dct = run_uncertainty_corr(rank_df_dct, kind)
     if save_sensitivity_excel: # too large, prefer not to do it
         export_to_excel(indicator_weights=True, mcda=True, uncertainty=False, sensitivity='Spearman')
     export_to_pickle(ahp=True, mcda=True, uncertainty=True, sensitivity='Spearman')
 
     kind = 'KS'
+    global rank_corr_dct
     rank_corr_dct = run_uncertainty_corr(rank_df_dct, kind)
     if save_sensitivity_excel: # too large, prefer not to do it
         export_to_excel(indicator_weights=False, mcda=False, uncertainty=False, sensitivity='KS')
